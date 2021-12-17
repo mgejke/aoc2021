dc26da15f082ae58872e66616889ce0b54b18adc
mgejke <martin@gejke.se>
2021-12-17 17:08:50 +0100

    Nicer solution

--- src/day17/main.rs
+++ src/day17/main.rs
@@ -7,64 +7,80 @@ fn sum_to_n(n: i32) -> i32 {
     n * (n + 1) / 2
 }
 
-fn p1_p2(input: Vec<String>) -> String {
+fn pos_after_n_iterations(initial: i32, iteration: i32, gravity: bool) -> i32 {
+    if !gravity && iteration >= initial {
+        sum_to_n(initial)
+    } else {
+        sum_to_n(initial) - sum_to_n(initial - iteration)
+    }
+}
+
+fn parse_input(input: Vec<String>) -> Vec<i32> {
     let re = Regex::new(r"-?\d+").unwrap();
-    let v: Vec<_> = re
-        .find_iter(&input[0])
+    re.find_iter(&input[0])
         .map(|v| v.as_str().parse::<i32>().unwrap())
-        .collect();
+        .collect()
+}
+
+fn p1(input: Vec<String>) -> String {
+    let v = parse_input(input);
+    let (y_min, y_max) = (v[2], v[3]);
+
+    let mut heights: Vec<i32> = Vec::new();
+    for v in 0..-y_min {
+        for y in 0..500 {
+            let pos_y = pos_after_n_iterations(v, y, true);
+            if pos_y >= y_min && pos_y <= y_max {
+                heights.push(sum_to_n(v));
+            }
+        }
+    }
+    format!("{}", heights.iter().max().unwrap())
+}
+
+fn p2(input: Vec<String>) -> String {
+    let v = parse_input(input);
     let (x_min, x_max, y_min, y_max) = (v[0], v[1], v[2], v[3]);
 
     let mut all: HashSet<(i32, i32)> = HashSet::new();
-    let mut heights: Vec<i32> = Vec::new();
-    for v in y_min..-y_min {
-        let height = std::cmp::max(0, sum_to_n(v));
-
-        for y in 0..200 {
-            let p = height - sum_to_n(y);
-
-            if p >= y_min && p <= y_max {
-                let iterations = v + y + 1;
-
+    for y in y_min..-y_min {
+        for i in 0..500 {
+            let y_pos = pos_after_n_iterations(y, i, true);
+            if y_pos >= y_min && y_pos <= y_max {
                 for x in 0..x_max + 1 {
-                    if iterations > x {
-                        let x_pos = sum_to_n(x);
-                        if x_pos >= x_min && x_pos <= x_max {
-                            heights.push(height);
-                            all.insert((x, v));
-                        }
-                    } else {
-                        let x_pos = sum_to_n(x) - sum_to_n(x - iterations);
-                        if x_pos >= x_min && x_pos <= x_max {
-                            heights.push(height);
-                            all.insert((x, v));
-                        }
+                    let x_pos = pos_after_n_iterations(x, i, false);
+                    if x_pos >= x_min && x_pos <= x_max {
+                        all.insert((x, y));
                     }
                 }
             }
         }
     }
-    println!("P1 - {:?}", heights.iter().max().unwrap());
     format!("{}", all.len())
 }
 
 fn main() {
     if let Ok(lines) = read_lines_to_strings("./data/input_day_17.txt") {
-        println!("P1 - {}", p1_p2(lines));
+        println!("P1 - {}", p1(lines));
+    }
+
+    if let Ok(lines) = read_lines_to_strings("./data/input_day_17.txt") {
+        println!("P2 - {}", p2(lines));
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::p1_p2;
-    // use crate::p2;
+    use crate::p1;
+    use crate::p2;
     const TEST_DATA: &[&str] = &["target area: x=20..30, y=-10..-5"];
 
     #[test]
     fn test_p1() {
-        assert_eq!(
-            p1_p2(TEST_DATA.iter().map(|s| s.to_string()).collect()),
-            "112"
-        );
+        assert_eq!(p1(TEST_DATA.iter().map(|s| s.to_string()).collect()), "45");
+    }
+    #[test]
+    fn test_p2() {
+        assert_eq!(p2(TEST_DATA.iter().map(|s| s.to_string()).collect()), "112");
     }
 }
